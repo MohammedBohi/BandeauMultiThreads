fb73747211b943b08bed96ccea9af67e8c3a6eb0
MohammedBohi <mohamedbohi2001@gmail.com>
2024-03-20 21:01:43 +0100

    Rendu TP

---
+++ src/main/java/bandeau/BandeauVerrouillable.java
@@ -0,0 +1,15 @@
+package bandeau;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class BandeauVerrouillable extends Bandeau {
+    private final Lock verrou = new ReentrantLock();
+    public void verrouille() {
+        verrou.lock();
+    }
+
+    public void deverrouille() {
+        verrou.unlock();
+    }
+}
---
+++ src/main/java/bandeau/Scenario.java
@@ -0,0 +1,74 @@
+package bandeau;
+import java.util.List;
+import java.util.LinkedList;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
+/**
+ * Classe utilitaire pour représenter la classe-association UML
+ */
+class ScenarioElement {
+
+    Effect effect;
+    int repeats;
+
+    ScenarioElement(Effect e, int r) {
+        effect = e;
+        repeats = r;
+    }
+}
+/**
+ * Un scenario mémorise une liste d'effets, et le nombre de repetitions pour chaque effet
+ * Un scenario sait se jouer sur un bandeau.
+ */
+public class Scenario {
+
+    private final ReentrantReadWriteLock rwl = new ReentrantReadWriteLock();
+    private final Lock verrouEnLecture = rwl.readLock();
+    private final Lock verrouEnEcriture = rwl.writeLock();
+
+    private final List<ScenarioElement> myElements = new LinkedList<>();
+
+    /**
+     * Ajouter un effect au scenario.
+     *
+     * @param e       l'effet à ajouter
+     * @param repeats le nombre de répétitions pour cet effet
+     */
+    public void addEffect(Effect e, int repeats) {
+        verrouEnEcriture.lock();
+        try {
+            myElements.add(new ScenarioElement(e, repeats));
+        } finally {
+            verrouEnEcriture.unlock();
+        }
+    }
+
+    /**
+     * Jouer ce scenario sur un bandeau
+     *
+     * @param b le bandeau ou s'afficher.
+     */
+    public void playOn(BandeauVerrouillable b) {
+        new Thread(
+                // "lambda-expression"
+                () -> {
+                    b.verrouille();
+                    try {
+                        play(b);
+                    } finally {
+                        b.deverrouille();
+                    }
+                }).start();
+    }
+
+    private void play(Bandeau b) {
+        verrouEnLecture.lock();
+        for (ScenarioElement element : myElements) {
+            for (int repeats = 0; repeats < element.repeats; repeats++) {
+                element.effect.playOn(b);
+            }
+        }
+        verrouEnLecture.unlock();
+    }
+}
